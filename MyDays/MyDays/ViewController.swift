d332fb4f4453a74d6b6c59a3ea3f2b7392a63f0a
YeoSeongil <seongil_yeo@naver.com>
2025-08-13 15:59:35 +0900

    프로젝트 생성

---
+++ MyDays/MyDays/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDays
+//
+//  Created by 여성일 on 8/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
